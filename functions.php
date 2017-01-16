631aa66ad3918d48e2768dc3e17f71e827eb4eea
root <root@v1o1.stratus8.com>
2017-01-15 23:04:55 -0500

    modified:   functions.php

--- functions.php
+++ functions.php
@@ -11,9 +11,21 @@ function search($needle, $heap){
 	$result[0]=$needle;
 	foreach($heap as $row){
 //		echo  strtoupper(preg_replace('/\s+/','',$row[0])) ."<br />";
-		if(!strcmp($searchneedle,strtoupper(preg_replace('/\s+/','',$row[0])))){
+		$rowcheck = strtoupper(preg_replace('/\s+/','',$row[0]));
+		if(!strcmp($searchneedle,$rowcheck)){
 			$result[0]= $row[0];
 			$result[1]=100;
+			echo "z";
+		}elseif(!strcmp($searchneedle,preg_replace('\'','',$rowcheck))){
+			$result[0]= $row[0];
+                        $result[1]=100;
+			echo "}" .$searchneedle .$rowcheck ."<br />";
+		}elseif(!strcmp($searchneedle,preg_replace('\'s','',$rowcheck))){
+                        $result[0]= $row[0];
+                        $result[1]=100;
+                }elseif(!strcmp($searchneedle,str_replace('&','AND', $rowcheck))){
+			$result[0]= $row[0];
+                        $result[1]=100;
 		}
 		
 	}
